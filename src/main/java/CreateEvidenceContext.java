2fdc656cd5043db8cb10a423a184d00c84c28cc6
sui-tyan <mallari23adrian@gmail.com>
2025-01-20 20:14:50 +0800

    Fix: jp characters turned garbage

--- src/main/java/CreateEvidenceContext.java
+++ src/main/java/CreateEvidenceContext.java
@@ -7,11 +7,11 @@ import burp.api.montoya.ui.contextmenu.ContextMenuItemsProvider;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class CreateEvidenceContext implements ContextMenuItemsProvider{
 
@@ -35,8 +35,11 @@ public class CreateEvidenceContext implements ContextMenuItemsProvider{
 
             createEvidenceItem.addActionListener(e -> {
                     JFileChooser fileChooser = new JFileChooser();
+                    FileNameExtensionFilter filter = new FileNameExtensionFilter("Text files", "txt");
+                    fileChooser.setFileFilter(filter);
                     fileChooser.setDialogTitle("Save As");
-                    fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+                    fileChooser.setSelectedFile(new File("_NoSignature_200_Normal.txt"));
+                    fileChooser.setCurrentDirectory(new File(System.getProperty("user.documents") + ".txt"));
 
                     int userSelection = fileChooser.showSaveDialog(null);
 
@@ -44,10 +47,14 @@ public class CreateEvidenceContext implements ContextMenuItemsProvider{
 
                         File fileToSave = fileChooser.getSelectedFile();
 
-                        try (FileWriter writer = new FileWriter(fileToSave)) {
-                            writer.write(requestResponse.request().toString() + "\n--------------------------------------------------\n" + requestResponse.response().toString());
-                            writer.close();
-                            api.logging().logToOutput(requestResponse.request().toString() + "\n--------------------------------------------------\n" + requestResponse.response().toString());
+                        try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(fileToSave), StandardCharsets.UTF_8);
+                             BufferedWriter bufferedWriter = new BufferedWriter(writer)) {
+
+                            String httpEvidenceDataRequest = new String(requestResponse.request().toByteArray().getBytes(), StandardCharsets.UTF_8);
+                            String httpEvidenceDataResponse = new String(requestResponse.response().toByteArray().getBytes(), StandardCharsets.UTF_8);
+
+                            bufferedWriter.write(httpEvidenceDataRequest + "\r\n\r\n--------------------------------------------------\r\n\r\n" + httpEvidenceDataResponse);
+                            bufferedWriter.close();
                             api.logging().logToOutput("File saved at: " + fileToSave.getAbsolutePath());
 
                         } catch (IOException exception) {
